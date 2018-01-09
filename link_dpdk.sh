9fa98c4e678901b927533dbf4e4c6f386af660d7
Mike Watson <watsonmf@gmail.com>
2018-01-08 17:30:17 -0700

    fix typo

--- link_dpdk.sh
+++ link_dpdk.sh
@@ -2,8 +2,8 @@
 
 DPDK_VERSION=dpdk-17.11
 
-ln -s "$PWD/$DPDK_VERSION/x86_64-native-linuxapp-gc/include" "$PWD/dpdk/include"
-ln -s "$PWD/$DPDK_VERSION/x86_64-native-linuxapp-gc/lib" "$PWD/dpdk/lib"
+ln -s "$PWD/$DPDK_VERSION/x86_64-native-linuxapp-gcc/include" "$PWD/dpdk/include"
+ln -s "$PWD/$DPDK_VERSION/x86_64-native-linuxapp-gcc/lib" "$PWD/dpdk/lib"
 
 
 #./configure --with-dpdk-lib="$PWD/dpdk"
