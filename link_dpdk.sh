4a4c7b05124812f42a9c20473b9a25d3fa964622
Mike Watson <watsonmf@gmail.com>
2018-03-20 15:34:46 -0600

    update link_dpdk.sh to dpdk-18.02

--- link_dpdk.sh
+++ link_dpdk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DPDK_VERSION=dpdk-17.11
+DPDK_VERSION=dpdk-18.02
 
 ln -s "$PWD/$DPDK_VERSION/x86_64-native-linuxapp-gcc/include" "$PWD/dpdk/include"
 ln -s "$PWD/$DPDK_VERSION/x86_64-native-linuxapp-gcc/lib" "$PWD/dpdk/lib"
